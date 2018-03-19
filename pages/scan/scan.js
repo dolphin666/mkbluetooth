ba039eeccb1462b52af155970ea5d32c1a77b8b4
dolphin <479019769zxx@gmail.com>
2018-03-19 18:04:41 +0800

    细节实现

--- pages/scan/scan.js
+++ pages/scan/scan.js
@@ -6,7 +6,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    tabArr: {
+      curHdIndex: 0,
+      curBdIndex: 0,
+      value:""
+    }, 
   },
 
   /**
@@ -14,8 +18,11 @@ Page({
    */
   onLoad: function () {
     const ctx = wx.createCanvasContext("myCanvas")
+    var minValue=0.75;
+    var maxValue=2.25;
+    var wValue=2.25-0.75;
     // 绘制一个灰色园
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI)
+    ctx.arc(130, 85, 80, 0, 2 * Math.PI)
     ctx.setFillStyle("#eee")
     ctx.fill()
 
@@ -25,20 +32,20 @@ Page({
     ctx.lineTo(160, 75)
     ctx.moveTo(100, 15)
     ctx.lineTo(100, 135)
-    ctx.setStrokeStyle("aaa")
-    ctx.stroke()
+    ctx.setStrokeStyle("#aaa")
+    // ctx.stroke()
 
     //坐标系4个点的数字
     ctx.setFontSize(12)
     ctx.setFillStyle("black")
-    ctx.fillText("0", 165, 78)
-    ctx.fillText("0.5 * PI", 83, 145)
-    ctx.fillText("1 * PI", 15, 78)
-    ctx.fillText("1.5 * PI", 83, 10)
+    // ctx.fillText("0", 165, 78)
+    // ctx.fillText("0.5 * PI", 83, 145)
+    // ctx.fillText("1 * PI", 15, 78)
+    // ctx.fillText("1.5 * PI", 83, 10)
 
     // 绘制圆心那个点
     ctx.beginPath()
-    ctx.arc(100, 75, 2, 0, 2 * Math.PI)
+    ctx.arc(130, 85, 5, 0, 2 * Math.PI)
     ctx.setFillStyle("lightgreen")
     ctx.fill()
 
@@ -46,23 +53,44 @@ Page({
     ctx.beginPath()
     ctx.arc(100, 25, 2, 0, 2 * Math.PI)
     ctx.setFillStyle("blue")
-    ctx.fill()
+    // ctx.fill()
 
     //绘制0的那个点
     ctx.beginPath()
     ctx.arc(150, 75, 2, 0, 2 * Math.PI)
     ctx.setFillStyle("red")
-    ctx.fill()
+    // ctx.fill()
 
     // 绘制黑线的弧度
     ctx.beginPath()
-    ctx.arc(100, 75, 50, 0, 1.5 * Math.PI)
-    ctx.setStrokeStyle("#333333")
+    ctx.arc(130, 85, 60, 0.75 * Math.PI, 2.25 * Math.PI)
+    ctx.setLineWidth(10);
+    ctx.setLineCap('round');
+    ctx.setStrokeStyle("#666")
+    ctx.stroke()
+
+    // 绘制亮色线的弧度
+    ctx.beginPath()
+    ctx.setLineWidth(6);
+    ctx.setLineCap('round');
+    ctx.setStrokeStyle("lightgreen")
+    ctx.arc(130, 85, 60, 0.75 * Math.PI, (0.75+1) * Math.PI)
     ctx.stroke()
 
     ctx.draw()
   },
-
+  tabFun: function (e) {
+    var that=this;
+    //获取触发事件组件的dataset属性  
+    var _datasetId = e.target.dataset.id;
+    console.log("----" + _datasetId + "----");
+    var _obj = {};
+    _obj.curHdIndex = _datasetId;
+    _obj.curBdIndex = _datasetId;
+    that.setData({
+      tabArr: _obj
+    });
+  }, 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
